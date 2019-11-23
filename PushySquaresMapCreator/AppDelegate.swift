13794d8a792ead232165c3a597be596a33d716cf
Mulang Su <sumulang@gmail.com>
2019-11-23 09:58:17 +0000

    Initial Commit

---
+++ PushySquaresMapCreator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PushySquaresMapCreator
+//
+//  Created by Mulang Su on 23/11/2019.
+//  Copyright © 2019 Mulang Su. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
