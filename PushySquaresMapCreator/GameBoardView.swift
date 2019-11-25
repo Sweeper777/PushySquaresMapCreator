4f81f586a04305fe34d1fb3394cb4d146d96aa7f
Mulang Su <sumulang@gmail.com>
2019-11-25 16:40:01 +0000

    copied the 5 helper properties and methods

--- PushySquaresMapCreator/GameBoardView.swift
+++ PushySquaresMapCreator/GameBoardView.swift
@@ -13,4 +13,26 @@ class GameBoardView : NSView {
     
     let borderSize: CGFloat = 8
     
+    
+    private func point(for position: Position) -> CGPoint {
+        return CGPoint(x: strokeWidth / 2 + position.x.f * squareLength, y: strokeWidth / 2 + position.y.f * squareLength)
+    }
+    
+    private var squareLength: CGFloat {
+        return (borderSize * self.width) / (borderSize * max(board.columns.f, board.rows.f) + 1.0)
+    }
+    
+    private var squareViewLength: CGFloat {
+        return squareLength - (squareLength / borderSize)
+    }
+    
+    private var strokeWidth: CGFloat {
+        return squareLength / borderSize
+    }
+    
+    func squareViewPoint(for position: Position) -> CGPoint {
+        let pointForPosition = point(for: position)
+        let offset = squareLength / borderSize / 2
+        return CGPoint(x: pointForPosition.x + offset, y: pointForPosition.y + offset)
+    }
 }
