e407bed516de32add63c5325126049b352d5404a
Mulang Su <sumulang@gmail.com>
2019-11-26 07:28:19 +0000

    tested a simple board

--- PushySquaresMapCreator/ViewController.swift
+++ PushySquaresMapCreator/ViewController.swift
@@ -7,6 +7,14 @@ class ViewController: NSViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var board = Array2D<Tile>(columns: 10, rows: 10, initialValue: .void)
+        for row in 1..<9 {
+            for column in 1..<9 {
+                board[row, column] = .empty
+            }
+        }
+
     }
 
 
