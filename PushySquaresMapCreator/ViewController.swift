f9bc71c53d62f2b02b440f44c12ac33fa8922de7
Mulang Su <sumulang@gmail.com>
2019-11-26 07:27:14 +0000

    added outlets in VC

--- PushySquaresMapCreator/ViewController.swift
+++ PushySquaresMapCreator/ViewController.swift
@@ -10,6 +10,9 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet var gameBoardView: GameBoardView!
+    @IBOutlet var gameBoardTextField: NSTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
