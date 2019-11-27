395fe90cbdd2697084d510d1b8055892dd449ce3
Mulang Su <sumulang@gmail.com>
2019-11-27 21:56:18 +0000

    added Map.init(fromString:)

--- PushySquaresMapCreator/Map.swift
+++ PushySquaresMapCreator/Map.swift
@@ -2,3 +2,20 @@
 
 typealias Map = Array2D<Tile>
 
+extension Array2D where T == Tile {
+    init?(fromString string: String) {
+        
+        let lines = string.split(separator: "\n").filter { $0 != "" }
+        guard lines.count > 0 else { return nil }
+        let rowCount = lines.count
+        let columnCount = lines.map { $0.count }.max()!
+        self.init(columns: rowCount, rows: columnCount, initialValue: .void)
+        for (x, line) in lines.enumerated() {
+            for (y, tileChar) in line.enumerated() {
+                guard let tile = Tile(rawValue: tileChar) else { return nil }
+                self[x, y] = tile
+            }
+        }
+    }
+    
+}
