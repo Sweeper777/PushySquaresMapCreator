3431f4cb39e69ab08093a1b0d74564fe47d5390f
Mulang Su <sumulang@gmail.com>
2019-11-27 21:59:41 +0000

    deleted the testing of GameBoardView

--- PushySquaresMapCreator/ViewController.swift
+++ PushySquaresMapCreator/ViewController.swift
@@ -8,28 +8,6 @@ class ViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var board = Array2D<Tile>(columns: 10, rows: 10, initialValue: .void)
-        for row in 1..<9 {
-            for column in 1..<9 {
-                board[row, column] = .empty
-            }
-        }
-
-        board[1, 1] = .spawn1
-        board[1, 8] = .spawn2
-        board[8, 8] = .spawn3
-        board[8, 1] = .spawn4
-
-        board[4, 4] = .wall
-        board[4, 5] = .wall
-        board[5, 5] = .wall
-        board[5, 4] = .wall
-
-        board[1, 2] = .slippery
-        board[1, 3] = .grey
-
-        gameBoardView.board = board
-        
     }
 
 
