3af97d427e20dff0a57480b5957205255171fe19
Mulang Su <sumulang@gmail.com>
2019-11-27 21:55:56 +0000

    given the tiles raw values

--- PushySquaresMapCreator/Tile.swift
+++ PushySquaresMapCreator/Tile.swift
@@ -1,11 +1,11 @@
-enum Tile {
-    case void
-    case empty
-    case wall
-    case grey
-    case slippery
-    case spawn1
-    case spawn2
-    case spawn3
-    case spawn4
+enum Tile: Character {
+    case void = "."
+    case empty = "+"
+    case wall = "O"
+    case grey = "g"
+    case slippery = "s"
+    case spawn1 = "1"
+    case spawn2 = "2"
+    case spawn3 = "3"
+    case spawn4 = "4"
 }
