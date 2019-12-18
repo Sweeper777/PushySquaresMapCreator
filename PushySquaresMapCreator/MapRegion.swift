a25324851f653dc7cf0d49d3a05ef7633ca1ac5e
Mulang Su <sumulang@gmail.com>
2019-12-18 13:10:12 +0800

    Create MapRegion.swift

---
+++ PushySquaresMapCreator/MapRegion.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+enum MapRegion {
+    case leftHalf
+    case rightHalf
+    case topHalf
+    case bottomHalf
+    case topLeftQuarter
+    case topRightQuarter
+    case bottomLeftQuarter
+    case bottomRightQuarter
+}
