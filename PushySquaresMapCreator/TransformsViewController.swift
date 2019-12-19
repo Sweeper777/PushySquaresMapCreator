ed68b85360497cbb3b96b1201383f2fad2fa9377
Mulang Su <sumulang@gmail.com>
2019-12-19 09:37:18 +0800

    Create TransformsViewController.swift

---
+++ PushySquaresMapCreator/TransformsViewController.swift
@@ -0,0 +1,11 @@
+import AppKit
+
+class TransformsViewController: NSViewController {
+    @IBOutlet var regionPopup: NSPopUpButton!
+    @IBOutlet var preview: GameBoardView!
+    
+    var originalMap: Map!
+    var transformedMap: Map!
+    
+}
+
