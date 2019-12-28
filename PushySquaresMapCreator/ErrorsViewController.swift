a44e8a6542f6f373e0d2ff357209f6690b076c2d
Mulang Su <sumulang@gmail.com>
2019-12-28 09:33:12 +0800

    set table view delegate and data source

--- PushySquaresMapCreator/ErrorsViewController.swift
+++ PushySquaresMapCreator/ErrorsViewController.swift
@@ -4,6 +4,13 @@ class ErrorsViewController: NSViewController {
     @IBOutlet var tableView: NSTableView!
     
     var errors: [MapError] = []
+    
+    override func viewDidLoad() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+
 }
 
 
