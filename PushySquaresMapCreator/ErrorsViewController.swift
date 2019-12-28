a4152f691cb1c1fac874cdfeada8b2f6d93230c0
Mulang Su <sumulang@gmail.com>
2019-12-28 09:33:22 +0800

    implemented table view data source

--- PushySquaresMapCreator/ErrorsViewController.swift
+++ PushySquaresMapCreator/ErrorsViewController.swift
@@ -11,6 +11,16 @@ class ErrorsViewController: NSViewController {
     }
 }
 
+extension ErrorsViewController : NSTableViewDelegate, NSTableViewDataSource {
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return errors.count
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "cell"), owner: nil) as? NSTableCellView
+        cell?.textField?.stringValue = errors[row].description
+        return cell
+    }
 }
 
 
